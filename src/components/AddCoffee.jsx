42fe46ab544fa49850dec8e81188ab1c40b4c8b4
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-05-16 05:03:32 +0600

    added post method

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -14,6 +14,19 @@ const AddCoffee = () => {
 
         const newCoffee = {name,quantity, supplier, taste, category, details, photo}
         console.log(newCoffee);
+        // send data to the server
+        fetch('http://localhost:5000/coffee', {
+            method:"POST",
+            headers:{
+                "content-type":"application/json"
+
+            },
+            body:JSON.stringify(newCoffee)
+        })
+        .then(res =>res.json())
+        .then(data => {
+            console.log(data);
+        })
     }
 
 
